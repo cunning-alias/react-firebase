13d695d208ba92eca25c5da8cdad866ece085e41
Dylan K <dylan@websummit.com>
2019-01-17 16:47:02 +0000

    s3 upload with node

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
-);
+class Landing extends Component {
+  constructor () {
+    super();
+    this.state = {
+      file: null
+    };
+  }
+
+  submitFile = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.file[0]);
+    axios.post(`/upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      console.log(response);
+      console.log(response.data.Location);
+    }).catch(error => {
+      console.log('error' + error.message);
+    });
+  }
+
+  handleFileUpload = (event) => {
+    this.setState({file: event.target.files});
+  }
+
+  render () {
+    return (
+      <form onSubmit={this.submitFile}>
+        <input label='upload file' type='file' onChange={this.handleFileUpload} />
+        <button type='submit'>Send</button>
+      </form>
+    );
+  }
+}
 
 export default Landing;
